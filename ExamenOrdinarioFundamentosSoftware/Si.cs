abd5a645f897b19e55a345cd0fb797f2a84fa828
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2023-12-18 21:30:50 -0600

    Si

---
+++ ExamenOrdinarioFundamentosSoftware/Si.cs
@@ -0,0 +1,15 @@
+namespace ExamenOrdinarioFundamentosSoftware
+{
+    internal class si
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // AAAAAAAAAAAAAAAAAAAAAAA
+            /// AAAAAAAAAAAAAAAAA
+            /// A
+            /// AS
+            /// DASDASPODKASPDKAS
+        }
+    }
+}
