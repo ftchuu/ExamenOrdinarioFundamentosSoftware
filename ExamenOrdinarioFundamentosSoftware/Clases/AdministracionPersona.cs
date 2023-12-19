1f1c7c96772a117caca32ab0a12725bad60fe337
Soff17K <sofitag09@gmail.com>
2023-12-19 11:39:11 -0600

    comportamiento complejo para ExaminarPersona

--- ExamenOrdinarioFundamentosSoftware/Clases/AdministracionPersona.cs
+++ ExamenOrdinarioFundamentosSoftware/Clases/AdministracionPersona.cs
@@ -43,5 +43,81 @@ namespace ExamenOrdinarioFundamentosSoftware.Clases
             }
         }
 
+        public void ExaminarPersona()
+        {
+            Console.WriteLine("¿Cuál es el id de la persona a examinar?");
+            int idExaminado = int.Parse(Console.ReadLine());
+            foreach (var persona in personasRegistradas)
+            {
+                if(persona.Id == idExaminado)
+                {
+                    Console.WriteLine($"{persona.Id} - {persona.Name}");
+                    if (persona.mascotas.Count == 0)
+                    {
+                        Console.WriteLine("Esta persona no posee mascotas");
+                    }
+                    else
+                    {
+                        foreach (var mascota in persona.mascotas)
+                        {
+                            Console.WriteLine($"{mascota.Id} - {mascota.Nombre} - {mascota.Especie}");
+                        }
+                    }
+                }
+                else
+                {
+                    bool buscar = true;
+                    while (true)
+                    {
+                        Console.WriteLine("¿Desea realizar una búsqueda por nombre?");
+                        Console.WriteLine("1)Si 2)No");
+                        string respuesta = Console.ReadLine();
+                        if (respuesta == "1")
+                        {
+                            Console.WriteLine("¿Cómo se llama la persona a buscar?");
+                            string nombrePersona = Console.ReadLine();
+                            foreach (var _persona in personasRegistradas)
+                            {
+                                if (_persona.Name.Contains(nombrePersona))
+                                {
+                                    Console.WriteLine($"{_persona.Id} - {_persona.Name}");
+                                    if (persona.mascotas.Count == 0)
+                                    {
+                                        Console.WriteLine("Esta persona no posee mascotas");
+                                    }
+                                    else
+                                    {
+                                        foreach (var mascota in persona.mascotas)
+                                        {
+                                            Console.WriteLine($"{mascota.Id} - {mascota.Nombre} - {mascota.Especie}");
+                                        }
+                                    }
+                                   
+                                }
+                                else
+                                {
+                                    Console.WriteLine("No se ha encontrado a la persona por ese nombre");
+                                    Console.WriteLine("¿Quiere intentarlo de nuevo?");
+                                    Console.WriteLine("1)Si 2)No");
+                                    string seleccion = Console.ReadLine();
+                                    if (seleccion == "1")
+                                    {
+                                        buscar = true;
+                                    }
+                                    else if (seleccion == "2")
+                                    {
+                                        buscar = false;
+                                        break;
+                                    }
+                                }
+
+                            }
+                        }
+                    }
+                    
+                }
+            }
+
+        }
     }
 }
