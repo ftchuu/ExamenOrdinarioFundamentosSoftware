a4b8b4e919e78cdd84fcb1b2a539e72cc5162198
Soff17K <sofitag09@gmail.com>
2023-12-19 11:54:37 -0600

    inicio del menu

--- ExamenOrdinarioFundamentosSoftware/Menu/Menu.cs
+++ ExamenOrdinarioFundamentosSoftware/Menu/Menu.cs
@@ -4,5 +4,7 @@ public class Class1
 {
 	public Class1()
 	{
-	}
+        Console.WriteLine("1)Administracion del centro 2)Administracion de adopciones 3)Administracion de bienestar animal 4)Simulacion de interacciones 5)Cerrar programa");
+
+    }
 }
