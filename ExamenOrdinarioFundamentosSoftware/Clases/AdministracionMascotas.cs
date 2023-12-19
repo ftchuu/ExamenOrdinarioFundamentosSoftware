277474862f49a4c3475e5f9388127f050c8dbdb8
ᓚᘏᗢ !? <datamayo.211@gmail.com>
2023-12-19 08:36:04 -0600

    Creación de la base del menú de "AdministraciónMascotas"
    Se creó lo que sería la base del menú, añadiendo todas las opciones de los posibles comportamientos que se esperan de esta clase en especifico de acuerdo a los requerimientos del pdf

---
+++ ExamenOrdinarioFundamentosSoftware/Clases/AdministracionMascotas.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenOrdinarioFundamentosSoftware.Clases
+{
+    public class AdminstracionMascotas
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Seleccione lo que quiere realizar");
+            System.Console.WriteLine("1 - Mostrar todas las mascotas registradas");
+            System.Console.WriteLine("2 - Registrar mascota nueva");
+            System.Console.WriteLine("3 - Buscar mascotas por especie");
+            System.Console.WriteLine("4 - Buscar mascotas por nombre");
+            System.Console.WriteLine("5 - Examinar mascota");
+            System.Console.WriteLine("6 - Volver al menú anterior");
+        }
+
+    }
+
+
+}
